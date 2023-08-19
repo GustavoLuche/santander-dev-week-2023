3005d48916cb0a749a057e9a4c75fe0fa8d1f931
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-08-18 21:57:05 -0300

    Add application-dev.yml, Repository, Update BaseItem

--- src/main/java/me/dio/domain/model/BaseItem.java
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -1,9 +1,19 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String icon;
+
     private String description;
 
     public Long getId() {
